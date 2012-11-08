6ed0db80445ec3abf16765fc37bf9c2b35c7e2bc
Dieter Giesler <dg@shopware.de>
2012-11-08 10:21:45 +0000

    Commit SwagHidePrices plugin.

---
+++ Meta.php
@@ -0,0 +1,23 @@
+<?php
+return array(
+	'version' => '1.0.2',
+	'autor' => 'shopware AG, d.giesler',
+	'copyright' => 'Copyright © 2012, shopware AG',
+	'label' => 'SwagHidePrices',
+	'name' => 'SwagHidePrices',
+	'description' => 'This plugin disables the displaying of article prices.',
+	'support' => 'http://forum.shopware.de',
+	'link' => 'http://www.shopware.de',
+	'changes' => array(
+		'1.0.0'=>array('releasedate'=>'2011-09-16', 'lines' => array(
+			'First release'
+		)),
+		'1.0.2'=>array('releasedate'=>'2012-10-15', 'lines' => array(
+			'Updated for Shopware 4.0'
+		)),
+		'1.0.3'=>array('releasedate'=>'2012-11-08', 'lines' => array(
+			'Fixed a model bug, so you can install the plugin'
+		))
+	),
+	'revision' => '2'
+);
